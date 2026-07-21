33542b89ff1563eaa02016ddc4cec0f828b0b72f
MaameOwusuaa <otrish95@gmail.com>
2026-07-21 11:02:04 +0000

    added java script file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Taking input from the user and storing it in a variable called  userName.
 let userName = prompt("Enter your name");
-// Prompt the user for their details
+// Prompt the user for their details.
 let age = prompt("Enter your age:");
 let email = prompt("Enter your email:");
 
